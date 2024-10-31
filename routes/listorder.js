048fe273e3f3577238f96f0db362bf665020b104
Rheiley <rheileyzoom@gmail.com>
2024-10-30 20:23:23 -0700

    Update listorder.js

--- routes/listorder.js
+++ routes/listorder.js
@@ -21,15 +21,15 @@ router.get('/', function(req, res, next) {
         try {
             let pool = await sql.connect(dbConfig);
 
-            let sqlQuery = "SELECT orderId, orderDate, customerId, customerName, totalAmount FROM ordersummary JOIN customer ON customer.customerId = ordersummary.customerId";
+            let sqlQuery = "SELECT orderId, orderDate, customerId, firstName, lastName, totalAmount FROM ordersummary JOIN customer ON customer.customerId = ordersummary.customerId";
             let results = await pool.request().query(sqlQuery);
             
             res.write("<table><tr><th>OrderId</th><th>Order Date</th><th>Customer Id</th><th>Customer Name</th><th>Total Amount</th></tr>");
             for (let i = 0; i < results.recordset.length; i++) {
                 let result = results.recordset[i];
-                res.write("<tr><td>" + result.orderId + "</td><td>" + result.orderDate + "</td><td>" + result.customerId + "</td><td>" + result.customerName + "</td><td>" + result.totalAmount + "</td></tr>");
+                res.write("<tr><td>" + result.orderId + "</td><td>" + result.orderDate + "</td><td>" + result.customerId + "</td><td>" + result.firstName + result.lastName + "</td><td>" + result.totalAmount + "</td></tr>");
                 
-                let sqlQuery2 = "SELECT productId, quantity, price FROM orderproduct WHERE orderId = {result.orderId}";
+                let sqlQuery2 = "SELECT productId, quantity, price FROM orderproduct WHERE orderId =" + result.orderId;
                 let result2 = await pool.request().query(sqlQuery2);
                 for(let j = 0; j < result2.recordset.length; j++){
                     let price = result2.price;
