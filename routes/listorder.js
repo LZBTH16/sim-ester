4544a146526f8c1ff9193d0e52ab575ab34d0b2a
Rheiley <rheileyzoom@gmail.com>
2024-10-30 20:48:41 -0700

    Update listorder.js

--- routes/listorder.js
+++ routes/listorder.js
@@ -21,20 +21,21 @@ router.get('/', function(req, res, next) {
         try {
             let pool = await sql.connect(dbConfig);
 
-            let sqlQuery = "SELECT orderId, orderDate, customerId, firstName, lastName, totalAmount FROM ordersummary JOIN customer ON customer.customerId = ordersummary.customerId";
+            let sqlQuery = "SELECT orderId, orderDate, customer.customerId, firstName, lastName, totalAmount FROM ordersummary JOIN customer ON customer.customerId = ordersummary.customerId";
             let results = await pool.request().query(sqlQuery);
             
-            res.write("<table><tr><th>OrderId</th><th>Order Date</th><th>Customer Id</th><th>Customer Name</th><th>Total Amount</th></tr>");
+            res.write("<table><tr><th>Order Id</th><th>Order Date</th><th>Customer Id</th><th>Customer Name</th><th>Total Amount</th></tr>");
             for (let i = 0; i < results.recordset.length; i++) {
                 let result = results.recordset[i];
-                res.write("<tr><td>" + result.orderId + "</td><td>" + result.orderDate + "</td><td>" + result.customerId + "</td><td>" + result.firstName + result.lastName + "</td><td>" + result.totalAmount + "</td></tr>");
+                res.write("<tr><td>" + result.orderId + "</td><td>" + moment(result.orderDate).format("YYYY-MM-DD HH:mm:ss.0") + "</td><td>" + result.customerId + "</td><td>" + result.firstName + " " + result.lastName + "</td><td>" + result.totalAmount + "</td></tr>");
                 
-                let sqlQuery2 = "SELECT productId, quantity, price FROM orderproduct WHERE orderId =" + result.orderId;
-                let result2 = await pool.request().query(sqlQuery2);
-                for(let j = 0; j < result2.recordset.length; j++){
-                    let price = result2.price;
-                    price = 
-                    res.write("<tr><td>" + result2.productId + "</td><td>" + result2.quantity + "</td><td>" + result2.price + "</td></tr>");
+                let sqlQuery2 = "SELECT productId, quantity, price FROM orderproduct WHERE orderId = " + result.orderId;
+                let results2 = await pool.request().query(sqlQuery2);
+                res.write("<table><tr><th>Product Id</th><th>Quantity</th><th>Price</th></tr>");
+                for(let j = 0; j < results2.recordset.length; j++){
+                    let result2 = results2.recordset[j];
+                    let price = result2.price.toFixed(2);
+                    res.write("<tr><td>" + result2.productId + "</td><td>" + result2.quantity + "</td><td>" + price + "</td></tr>");
                 }
             }
             res.write("</table>");
