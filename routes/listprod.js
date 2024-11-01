30990b88ac995b9a9496d7bbc89247e48d311ddf
Liz <lizrd2016@gmail.com>
2024-11-01 12:46:53 -0700

    Implement listprod.js functionality

--- routes/listprod.js
+++ routes/listprod.js
@@ -32,25 +32,37 @@ router.get('/', function(req, res, next) {
         res.write('<input type="text" name="productName" size="50">');
         res.write('<input type="submit" value="Submit"><input type="reset" value="Reset">');
         res.write('</form>');
-
-        // getting the product name from the string query
-        if(!name){
-            res.end();
-            return;
-        }
         
         (async function() {
             try {
                 let pool = await sql.connect(dbConfig);
     
-                let sqlQuery = "SELECT productName, productPrice FROM product WHERE productName LIKE @name";              
-                let results = await pool.request().input('name', sql.VarChar, '%{productName}%').query(sqlQuery);
+                let sqlQuery;
+                if(!name){
+                    sqlQuery = "SELECT productId, productName, productPrice FROM product"; // default, when not specifically searching
+                } else {
+                    sqlQuery = "SELECT productId, productName, productPrice FROM product WHERE productName LIKE @name"; // when searching for something
+                }
+                
+                let results;
+                if(name){
+                    results = await pool.request().input('name', sql.VarChar, `%${name}%`).query(sqlQuery); // results should be the specified search
+                } else {
+                    results = await pool.request().query(sqlQuery); // all products
+                }
+
+                // displaying the search message
+                if(name){
+                    res.write(`<h2>Products containing '${name}'</h2>`);
+                } else{
+                    res.write("<h2>All Products</h2>");
+                }
                 
                 res.write("<table><tr><th></th><th>Product Name</th><th>Price</th></tr>");
                 for (let i = 0; i < results.recordset.length; i++) {
                     let result = results.recordset[i];
                     let price = "$"+result.productPrice.toFixed(2);
-                    res.write("<tr><td><a href='addcart?id=<productId>&name=<productName>&price=<productPrice>'>Add to cart</a></td><td>" + result.productName + "</td><td>" + price + "</td></tr>");
+                    res.write(`<tr><td><a href='addcart?id=${result.productId}&name=${result.productName}&price=${result.productPrice}'>Add to cart</a></td><td>${result.productName}</td><td>${price}</td></tr>`);
 
                 }
                 res.write("</table>");
