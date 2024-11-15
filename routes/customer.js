11ad49f958638bef42fbedc40111b5b37a8f147f
Liz <lizrd2016@gmail.com>
2024-11-15 14:28:24 -0800

    Implement Customer.js functionality

--- routes/customer.js
+++ routes/customer.js
@@ -4,6 +4,8 @@ const sql = require('mssql');
 const auth = require('../auth');
 
 router.get('/', function(req, res, next) {
+    auth.checkAuthentication(req, res); // display error msg if attempting to access page and not logged in
+    const username = req.session.authenticatedUser;
 
     res.setHeader('Content-Type', 'text/html');
 
@@ -13,6 +15,19 @@ router.get('/', function(req, res, next) {
         try {
 
 	// TODO: Print customer info
+
+            const pool = await sql.connect(dbConfig);
+
+            // grabbing everything but the password
+            const sqlQuery = "SELECT customerId, firstName, lastName, email, phonenum, address, city, state, postalCode, country, userid FROM customer WHERE userid LIKE @username";
+            const result = await pool.request().input('username', sql.VarChar, username).query(sqlQuery);
+
+            // grabbing the first (and only) part of the queried array to be displayed on the site
+            const customer = result.recordset[0];
+
+            // send the query to be displayed
+            res.render('customer', {customer});
+
         } catch(err) {
             console.dir(err);
             res.write(err + "")
