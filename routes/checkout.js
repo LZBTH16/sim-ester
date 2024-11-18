2c63d696cd450c7e00057a067ab47dcedff80b01
Liz <lizrd2016@gmail.com>
2024-11-17 17:58:41 -0800

    Add checkout CSS

--- routes/checkout.js
+++ routes/checkout.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('checkout'); 
+    res.render('checkout', {
+        title: "SIM-ESTER Checkout Line"
+    }); 
 });
 
 module.exports = router;
