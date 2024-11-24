c5c0c53fd25551adc16838cbe56b1666d09476fd
Liz <lizrd2016@gmail.com>
2024-11-24 15:50:22 -0800

    Add access to customer.handlebars

--- routes/login.js
+++ routes/login.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
     }
 
     res.render('login', {
-        title: "Login Screen",
+        title: "Login",
         loginMessage: loginMessage
     });
 });
