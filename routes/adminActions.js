0ec6c1953afe321f32b2fb35a9d69574300a8b1a
Liz <lizrd2016@gmail.com>
2024-11-30 12:45:34 -0800

    Implement delete/add product for admin

--- routes/adminActions.js
+++ routes/adminActions.js
@@ -71,8 +71,19 @@ router.post('/deleteProduct', async function (req, res, next) {
     try {
         const pool = await sql.connect(dbConfig);
 
+        // need to delete from the other tables first
+        const deleteOrderProduct = "DELETE FROM orderproduct WHERE productId = @productId";
+        await pool.request().input('productId', sql.Int, productId).query(deleteOrderProduct);
+        const deleteProductInventory = "DELETE FROM productinventory WHERE productId = @productId";
+        await pool.request().input('productId', sql.Int, productId).query(deleteProductInventory);
+
+        // now to delete from the product table
         const deleteQuery = "DELETE FROM product WHERE productId = @productId";
-        await pool.request().input('productId', sql.Int, productId).query(deleteQuery);        
+        await pool.request().input('productId', sql.Int, productId).query(deleteQuery);
+        
+        req.session.successMessage = 'Product deleted successfully!';
+
+        res.redirect('/adminActions');
     } catch (err) {
         console.error(err);
         res.write(err + "")
@@ -82,7 +93,30 @@ router.post('/deleteProduct', async function (req, res, next) {
 
 // to add a product
 router.post('/addProduct', async function (req, res, next) {
+    auth.checkAuthentication(req, res);
 
+    const newData = req.body;
+    const {productName, productPrice, productDesc, categoryId} = newData;
+
+    try {
+        const pool = await sql.connect(dbConfig);
+        
+        const addQuery = "INSERT INTO product(productName, categoryId, productDesc, productPrice) VALUES (@productName, @categoryId, @productDesc, @productPrice)";
+        await pool.request()
+        .input('productName', sql.VarChar, productName)
+        .input('categoryId', sql.Int, categoryId)
+        .input('productDesc', sql.VarChar, productDesc)
+        .input('productPrice', sql.Decimal, productPrice)
+        .query(addQuery);
+    
+        req.session.successMessage = 'Product added successfully!';
+
+        res.redirect('/adminActions');
+    } catch (err) {
+        console.error(err);
+        res.write(err + "")
+        res.end();
+    }
 });
 
 module.exports = router;
