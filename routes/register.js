b1822661af2fa6dcdc693edaa5430d7ca1b8b2ae
Rheiley <118871025+Rheiley@users.noreply.github.com>
2024-12-02 10:25:56 -0800

    Add list of countries to register.handlebars

--- routes/register.js
+++ routes/register.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.setHeader('Content-Type', 'text/html');
+
     // Render the register form
     res.render('register', {
         title: "Register"
