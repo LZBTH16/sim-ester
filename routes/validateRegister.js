24e2f91a95a75792c2ba69bfb3033658ed1cf392
Rheiley <rheileyzoom@gmail.com>
2024-12-01 19:25:26 -0800

    Update validateRegister.js

--- routes/validateRegister.js
+++ routes/validateRegister.js
@@ -31,7 +31,8 @@ router.post('/', function(req, res) {
         }
 
         // Check if phone number is valid
-        const phoneRegex = /^\+?[1-9]\d{0,2}[-.\s]?[(]?\d{3}[)]?[-.\s]?\d{3}[-.\s]?\d{4}$/;
+        const phoneRegex = /^\+?[1-9]\d{0,2}[-.\s]?(\(?\d{1,4}\)?)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+
 
         if(!phoneRegex.test(phone)){
             return res.render('registrationError', { invalidPhoneNumber: true })
