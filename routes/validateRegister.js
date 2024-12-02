bfcda8e13c98d820192b45b695b594283fb82977
Rheiley <rheileyzoom@gmail.com>
2024-12-01 19:41:44 -0800

    Fix phone validation

--- routes/validateRegister.js
+++ routes/validateRegister.js
@@ -31,7 +31,8 @@ router.post('/', function(req, res) {
         }
 
         // Check if phone number is valid
-        const phoneRegex = /^\+?[1-9]\d{0,2}[-.\s]?(\(?\d{1,4}\)?)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+        // const phoneRegex = /^\+?[1-9]\d{0,2}[-.\s]?(\(?\d{1,4}\)?)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+        const phoneRegex = /^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
 
         if(!phoneRegex.test(phone)){
             return res.render('registrationError', { invalidPhoneNumber: true });
