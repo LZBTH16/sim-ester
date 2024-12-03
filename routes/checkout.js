434d4ca25b261d46c878700e68bf8f0b8677f278
Rheiley <118871025+Rheiley@users.noreply.github.com>
2024-12-03 11:22:53 -0800

    Add data validation in checkout and showcart

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../auth');
 
 router.get('/', function(req, res, next) {
+    auth.checkAuthentication(req, res);
+
     res.render('checkout', {
         title: "SIM-ESTER Checkout",
         username: req.session.authenticatedUser
